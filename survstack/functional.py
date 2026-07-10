4f19190e5b93251b62e2d58494c66cffa178a48f
Marco BARBERO MOTA <marco.barberomota@ekimetrics.com>
2026-07-10 20:37:25 +0200

    Make normalization of evaluation times optional (default:False)

--- survstack/functional.py
+++ survstack/functional.py
@@ -128,9 +128,11 @@ def _encode_onehot(times: np.ndarray, n_samples: int) -> np.ndarray:
     return np.tile(np.eye(times.shape[0]), (n_samples, 1))
 
 
-def _encode_continuous(times: np.ndarray, n_samples: int) -> np.ndarray:
-    """Tile a normalized time vector for continuous time encoding."""
-    return np.tile(times / times[-1], n_samples).reshape(-1, 1)
+def _encode_continuous(times: np.ndarray, n_samples: int, normalize: bool = True) -> np.ndarray:
+    """Tile a (optionally normalized) time vector for continuous time encoding."""
+    if normalize:
+        times = times / times[-1]
+    return np.tile(times, n_samples).reshape(-1, 1)
 
 
 def cumulative_hazard_function(estimates: np.ndarray, times: np.ndarray) -> np.ndarray:
